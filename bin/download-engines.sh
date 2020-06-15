dde41979b76424bfd1e97087a9f39ae82848bfe0
Federico Frigo <ffrigo8@gmail.com>
2020-06-15 09:56:17 +0200

    Fix curl expired certificates error
    - Added `-k` flag to enable unsecure connection to server

--- bin/download-engines.sh
+++ bin/download-engines.sh
@@ -1,23 +1,23 @@
 [ ! -d "engines" ] && mkdir engines
 cd engines
 
-curl -O https://artifacts.elastic.co/downloads/logstash/logstash-7.7.0.tar.gz
+curl -O -k https://artifacts.elastic.co/downloads/logstash/logstash-7.7.0.tar.gz
 tar -xvzf logstash-7.7.0.tar.gz
 rm logstash-7.7.0.tar.gz
 
-curl -O https://mirror.nohup.it/apache/kafka/2.5.0/kafka_2.12-2.5.0.tgz
+curl -O -k https://mirror.nohup.it/apache/kafka/2.5.0/kafka_2.12-2.5.0.tgz
 tar -xvzf kafka_2.12-2.5.0.tgz
 rm kafka_2.12-2.5.0.tgz
 
-curl -O https://mirror.nohup.it/apache/spark/spark-2.4.5/spark-2.4.5-bin-hadoop2.7.tgz
+curl -O -k https://mirror.nohup.it/apache/spark/spark-2.4.5/spark-2.4.5-bin-hadoop2.7.tgz
 tar -xvzf spark-2.4.5-bin-hadoop2.7.tgz
 rm spark-2.4.5-bin-hadoop2.7.tgz
 
-curl -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.7.0-linux-x86_64.tar.gz
+curl -O -k https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.7.0-linux-x86_64.tar.gz
 tar -xvzf elasticsearch-7.7.0-linux-x86_64.tar.gz
 rm elasticsearch-7.7.0-linux-x86_64.tar.gz
 
-curl -O https://artifacts.elastic.co/downloads/kibana/kibana-7.7.0-linux-x86_64.tar.gz
+curl -O -k https://artifacts.elastic.co/downloads/kibana/kibana-7.7.0-linux-x86_64.tar.gz
 tar -xvzf kibana-7.7.0-linux-x86_64.tar.gz
 rm kibana-7.7.0-linux-x86_64.tar.gz
 
